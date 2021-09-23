24d325c4bebc1c9d3994f193f3a5a3877a6e87f3
marli99 <180234@virtualwindow.co.za>
2021-09-23 09:32:07 +0200

    Initial commit

---
+++ Postaholic/PostaholicApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostaholicApp.swift
+//  Postaholic
+//
+//  Created by Marli Correia on 2021/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PostaholicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
